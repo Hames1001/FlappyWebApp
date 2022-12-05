47774f3d06c939a7338008d43906de047c5fd82e
James <jamesrazo12@gmail.com>
2022-12-05 13:04:36 -0800

    installing packages

---
+++ flappyBird/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import styled from styled-components;
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App ">
+      Test
+    </div>
+  )
+}
+
+export default App
+
+const Bird = styled.div`
+  position: absolute;
+`;
+
