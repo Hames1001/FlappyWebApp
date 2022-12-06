f35ddc9dd8fcbd8799099eb90b19b6917c38adf1
James <jamesrazo12@gmail.com>
2022-12-06 10:00:44 -0800

    Bird is falling

--- flappyBird/src/App.jsx
+++ flappyBird/src/App.jsx
@@ -1,13 +1,37 @@
-import './App.css'
-import styled from styled-components;
+//import './App.css'
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+
+const BIRD_SIZE = 20;
+const GAME_WIDTH = 500;
+const GAME_HEIGHT = 500;
+const GRAVITY = 3;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [birdPosition, setBirdPosition] = useState(250);
+
+  useEffect(() => {
+    
+    let timeId;
+    if (birdPosition < GAME_HEIGHT - BIRD_SIZE) {
+      timeId = setInterval(() => {
+        setBirdPosition(birdPosition => birdPosition+GRAVITY)
+      }, 24)
+    }
+
+    return () => {
+      clearInterval(timeId);
+    };
+  });
+
 
   return (
-    <div className="App ">
-      Test
-    </div>
+    <Div>
+      <GameBox height={GAME_HEIGHT} width={GAME_WIDTH}>
+        <Bird size={BIRD_SIZE} top={birdPosition}/>
+      </GameBox>
+    </Div>
+    
   )
 }
 
@@ -15,5 +39,21 @@ export default App
 
 const Bird = styled.div`
   position: absolute;
+  background-color: red;
+  height: ${(props) => props.size}px;
+  width: ${(props) => props.size}px;
+  top: ${(props) => props.top}px;
+  border-radius: 50%;
 `;
 
+const Div = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`;
+
+const GameBox = styled.div`
+  height: ${(props) => props.height}px;
+  width: ${(props) => props.width}px;
+  background-color: blue;
+`;
